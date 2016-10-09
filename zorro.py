c82c6315bc9a68eb6beb86007a92ef2f54442aec
Hebron George <hebrontgeorge@gmail.com>
2016-10-09 15:52:13 -0400

    made zorro be callable

--- zorro.py
+++ zorro.py
@@ -1,13 +1,17 @@
 import csv
-f = open('names_20161004.csv', 'r')
-try:
-	reader = csv.reader(f)
-	for row in reader:
-		# Using the requests module
-		# iterate through each row and pull info
-		print row[0]
-		pass
-except Exception as e:
-	print "Error reading from csv file: {}".format(e)
-finally:
-	f.close()
+def main():
+	f = open('names_20161004.csv', 'r')
+	try:
+		reader = csv.reader(f)
+		for row in reader:
+			# Using the requests module
+			# iterate through each row and pull info
+			print row[0]
+			pass
+	except Exception as e:
+		print "Error reading from csv file: {}".format(e)
+	finally:
+		f.close()
+
+if __name__ == "__main__":
+	main()
